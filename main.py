59c95ad5062daac09b5ed3fb33168b13aa4ebb49
KAlbert <albertkorol2501@gmail.com>
2020-04-24 13:00:08 +0000

    Created JSON system

--- main.py
+++ main.py
@@ -14,9 +14,33 @@ def getReturnOk():
 def returnHomeHtml():
   return render_template("home.html")
 
-@app.route('/calc')
+@app.route('/calc' , methods = ["POST"])
 def returnCalcHtml():
   return render_template("calc.html")
+  sk1 = int(input())
+  sk2 = int(input())
+  darb = str(input())
+  JSON = {
+    "sk1" :  sk1
+    "sk2" :  sk2
+    "darb" :  darb
+  }
+
+@app.route('/getResults', methods = ["GET"])  
+def getResults():
+  if darb == "+":
+    actionReslt = JSON["sk1"] + JSON["sk2"] 
+  elif darb == "-":
+     actionReslt = JSON["sk1"] - JSON["sk2"] 
+  elif darb == "*":
+    actionReslt = JSON["sk1"] * JSON["sk2"] 
+  elif darb == "/":
+    actionReslt = JSON["sk1"] / JSON["sk2"]
+  JSON1 = {
+    "rez" : actionReslt 
+  }
+
+  print(JSON1["rez"])
 
 @app.route('/about')
 def returnAboutHtml():
