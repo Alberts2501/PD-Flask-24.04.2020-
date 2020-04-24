847fb9f00e84883e2470c4cb0f9d00c8c6cbf993
KAlbert <albertkorol2501@gmail.com>
2020-04-24 12:00:09 +0000

    Route and render_template for home/calc.html created

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def getIndex():
 def getReturnOk():
   return "OK"
 
+@app.route('/home')
+def returnHomeHtml():
+  return render_template("home.html")
+
+@app.route('calc.html')
+def returnCalcHtml():
+  return render_template("calc.html")
+
 if __name__ == '__main__':
   app.run(threaded = True, port = 5000, debug = True)
