6bf04e5944f85ef370ada060872810db806be474
KAlbert <albertkorol2501@gmail.com>
2020-04-24 12:23:43 +0000

    Created about.html. Modified with link to about.html. Before all it was created /about route.

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ def getReturnOk():
 def returnHomeHtml():
   return render_template("home.html")
 
-@app.route('calc.html')
+@app.route('/calc')
 def returnCalcHtml():
   return render_template("calc.html")
+
+@app.route('/about')
+def returnAboutHtml():
+  return render_template("about.html")
 
 if __name__ == '__main__':
   app.run(threaded = True, port = 5000, debug = True)
